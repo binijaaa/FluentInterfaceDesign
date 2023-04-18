2146c9a9d68b9be111438892f22a3bfb07581879
binijaaa <sabine.kolesnikova@gmail.com>
2023-04-18 19:41:40 +0300

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentInterfaceDesign
+{
+    internal class Employee
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+
+namespace FluentInterfaceDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("000000000000000000000000000000");
+
+            int[] someNumbers = { 4, 3, 2, 1 };
+
+            //chaining that comes together with LINQ framework
+
+            //variable where this chain method will create an output:
+
+            var processedValues = someNumbers
+                .Select(n => n * 10) //left side each represented element. On right - representing statement --what we want to do with this element
+                .Where(n => n > 10); //allowing to do check of our elements, output as a condition if - then true
+
+
+
+            //this is the same as:
+            foreach (int n in someNumbers)
+            {
+                Console.WriteLine(n * 10);
+            }
+            //HOWEVER, we also need an IF statement and that would be hard to implement here!
+            Console.WriteLine("000000000000000000000000000000");
+
+
+            ShowElements(processedValues);
+
+            Console.WriteLine("000000000000000000000000000000");
+
+
+            FluentEmployee employeeObject = new FluentEmployee();
+
+            employeeObject
+                .NameOfEmployee("Anthony Finn")
+                .EmployeeDateOfBirth("12/09/1992")
+                .EmployeeDepartment("IT")
+                .EmployeeAddress("5th Avenue");
+
+            employeeObject.ShowDetailsOfEmployee();
+
+        }
+
+        static void ShowElements(IEnumerable<int> values)
+        {
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+
+
+    }
+}
